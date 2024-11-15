3bedad6c5049211530fb721e19d383cd94da573c
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-11-15 14:26:13 +0200

    tee greeting metodi, joka kysyy käyttäjältä nimen ja tervehtii

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        greeting();
+    }
+
+    public static void greeting() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Who are you?");
+        String name = in.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
