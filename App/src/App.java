583e9f0d8cde0c02fda5e62744d6cd3dbb39dd15
LottaSofiaK <lotta.kauppinen@student.hamk.fi>
2024-11-19 12:37:58 +0200

    Muodostetaan käyttäjänimi lempiväristä ja lempieläimestä.

--- App/src/App.java
+++ App/src/App.java
@@ -1,15 +1,31 @@
 import java.util.Scanner;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
         greeting();
+        createUsername();
     }
 
+    // Tarvittavat tiedot.
+    static Scanner in = new Scanner(System.in);
+    static String name;
+    static String colour;
+    static String animal;
+
     public static void greeting() {
-        Scanner in = new Scanner(System.in);
         System.out.println("Who are you?");
-        String name = in.nextLine();
+        name = in.nextLine();
         System.out.println("Hello " + name + "!");
+
     }
+
+    public static void createUsername() {
+        System.out.println("Let's create a username for you. Tell me your favourite colour.");
+        String colour = in.nextLine();
+        System.out.println("Now tell me your favourite animal.");
+        String animal = in.nextLine();
+        String username = (name + "The" + colour + animal).toLowerCase();
+        System.out.println("Your username is " + username + "!");
+    }
+
 }
